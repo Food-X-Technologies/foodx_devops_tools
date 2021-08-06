13673417afe4cb728557a1c442c222a79427e1fb
Russell Smiley <russell@foodxtech.com>
2021-08-06 13:25:03 -0700

    Add --validation option. implement initial clean deploy-me that does nothing.

--- foodx_devops_tools/deploy_me/_main.py
+++ foodx_devops_tools/deploy_me/_main.py
@@ -29,6 +29,12 @@ from foodx_devops_tools.release_flow import (
 )
 from foodx_devops_tools.utility import run_command
 
+from ._deployment import (
+    DeploymentState,
+    FlattenedDeployment,
+    assess_results,
+    do_deploy,
+)
 from ._state import ExitState
 
 log = logging.getLogger(__name__)
@@ -39,6 +45,7 @@ class DeploymentConfigurationError(Exception):
 
 
 def _get_sha() -> str:
+    """Get the git commit SHA of HEAD."""
     result = run_command(
         ["git", "rev-parse", "HEAD"], text=True, capture_output=True
     )
@@ -49,6 +56,20 @@ def _get_sha() -> str:
         this_sha = result.stdout[0:10]
 
     return this_sha
+
+
+async def _gather_main(
+    configuration: PipelineConfiguration,
+    enable_validation: bool,
+    deployment_iterations: typing.List[FlattenedDeployment],
+) -> typing.List[DeploymentState]:
+    result = await asyncio.gather(
+        *[
+            do_deploy(configuration, x, enable_validation)
+            for x in deployment_iterations
+        ]
+    )
+    return result
 
 
 @click.command()
@@ -64,7 +85,14 @@ def _get_sha() -> str:
 @click.option(
     "--git-ref",
     default=None,
-    help="Git reference to drive release management workflow",
+    help="""Git reference to drive release management workflow.
+
+eg.
+
+    ``$(Build.SourceBranch)`` for Azure DevOps Pipelines.
+    ``${GITHUB_REF}`` for Github Actions.
+    ``${CI_COMMIT_REF_NAME}`` for Gitlab-CI.
+""",
     type=str,
 )
 @click.option(
@@ -73,16 +101,26 @@ def _get_sha() -> str:
     help="""The pipeline ID. Should be considered mandatory for a CI/CD
  pipeline (optional for developers running locally).
 
-  eg.``$(Build.BuildNumber)`` for Azure DevOps Pipelines.
-     ``${GITHUB_RUN_NUMBER}`` for Github Actions.
-     ``${CI_PIPELINE_ID}`` for Gitlab-CI.""",
+eg.
+
+    ``$(Build.BuildNumber)`` for Azure DevOps Pipelines.
+    ``${GITHUB_RUN_NUMBER}`` for Github Actions.
+    ``${CI_PIPELINE_ID}`` for Gitlab-CI.""",
     type=str,
+)
+@click.option(
+    "--validation",
+    default=False,
+    help="Force deployments to be a validation deployment, regardless of any "
+    "specified deployment mode in configuration.",
+    is_flag=True,
 )
 def deploy_me(
     client_config: pathlib.Path,
     system_config: pathlib.Path,
     git_ref: typing.Optional[str],
     pipeline_id: str,
+    validation: bool,
 ) -> None:
     """
     Deploy system resources.
@@ -118,6 +156,22 @@ def deploy_me(
         deployment_iterations = pipeline_state.flatten()
 
         log.debug(str(deployment_iterations))
+
+        results = asyncio.run(
+            _gather_main(this_configuration, validation, deployment_iterations)
+        )
+        condensed_result = assess_results(results)
+        if condensed_result.code == DeploymentState.ResultType.success:
+            click.echo(
+                click.style("success: Deployment succeeded.", fg="green")
+            )
+        else:
+            click.echo(
+                click.style(
+                    "FAILED: Deployment failed. Check log for " "details.",
+                    fg="red",
+                )
+            )
     except (ConfigurationPathsError, DeploymentConfigurationError) as e:
         message = str(e)
         log.error(message)
