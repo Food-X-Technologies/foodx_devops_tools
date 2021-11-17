9f427aefebf738bbb96199ff442970cd10de18f9
Russell Smiley <russell@foodxtech.com>
2021-11-17 21:32:20 +0000

    add optional branch field to deployment subscription pipelien config

--- foodx_devops_tools/pipeline_config/deployments.py
+++ foodx_devops_tools/pipeline_config/deployments.py
@@ -28,6 +28,7 @@ class DeploymentLocations(pydantic.BaseModel):
 class DeploymentSubscriptionReference(pydantic.BaseModel):
     """A subscription reference in a deployment definition."""
 
+    branch: typing.Optional[typing.List[str]]
     locations: typing.List[DeploymentLocations]
     root_fqdn: str
 
